b22359dea97a25b8667a8677e390c6a37426d2da
Jason Awbrey <jawbrey@gmail.com>
2024-04-27 15:16:23 -0500

    footer updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,7 @@ import { Link } from 'gatsby';
 import logo from '../img/iona-logo.png';
 import facebook from '../img/social/facebook.svg';
 import instagram from '../img/social/instagram.svg';
-import twitter from '../img/social/twitter.svg';
-import vimeo from '../img/social/vimeo.svg';
+import youtube from '../img/social/youtube.svg';
 
 const Footer = () => {
   return (
@@ -16,64 +15,28 @@ const Footer = () => {
       <div className="content has-text-centered has-background-black has-text-white-ter">
         <div className="container has-background-black has-text-white-ter">
           <div style={{ maxWidth: '100vw' }} className="columns">
-            <div className="column is-4">
-              <section className="menu">
-                <ul className="menu-list">
-                  <li className="navbar-item" style={{ padding: '0px' }}>
-                    <Link className="navbar-item" to="/concerts">
-                      Concerts
-                    </Link>
-                  </li>
-                  <li className="navbar-item" style={{ padding: '0px' }}>
-                    <Link className="navbar-item" to="/about">
-                      About
-                    </Link>
-                  </li>
-                  <li className="navbar-item" style={{ padding: '0px' }}>
-                    <Link className="navbar-item" to="/media">
-                      Media
-                    </Link>
-                  </li>
-                  <li className="navbar-item" style={{ padding: '0px' }}>
-                    <Link className="navbar-item" to="/contact">
-                      Contact
-                    </Link>
-                  </li>
-                  <li className="navbar-item" style={{ padding: '0px' }}>
-                    <Link className="navbar-item" to="/give">
-                      Give
-                    </Link>
-                  </li>
-                </ul>
-              </section>
-            </div>
-            <div className="column is-4 social">
-              <a title="facebook" href="https://facebook.com">
+            <div className="column is-12 social">
+              <a title="facebook" href="https://www.facebook.com/EnsembleIona">
                 <img
                   src={facebook}
                   alt="Facebook"
                   style={{ width: '1em', height: '1em' }}
                 />
               </a>
-              <a title="twitter" href="https://twitter.com">
-                <img
-                  className="fas fa-lg"
-                  src={twitter}
-                  alt="Twitter"
-                  style={{ width: '1em', height: '1em' }}
-                />
-              </a>
-              <a title="instagram" href="https://instagram.com">
+              <a
+                title="instagram"
+                href="https://www.instagram.com/ensembleiona/"
+              >
                 <img
                   src={instagram}
                   alt="Instagram"
                   style={{ width: '1em', height: '1em' }}
                 />
               </a>
-              <a title="vimeo" href="https://vimeo.com">
+              <a title="youtube" href="https://www.youtube.com/@EnsembleIona">
                 <img
-                  src={vimeo}
-                  alt="Vimeo"
+                  src={youtube}
+                  alt="Youtube"
                   style={{ width: '1em', height: '1em' }}
                 />
               </a>
