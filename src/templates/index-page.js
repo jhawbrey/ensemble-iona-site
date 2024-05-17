1033ad8fc8e13f509785f3531f4730a7a77d7ae2
Jason Awbrey <jawbrey@gmail.com>
2024-05-17 08:10:42 -0500

    home page titles update

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -39,7 +39,7 @@ export const IndexPageTemplate = ({
                   <div className="columns">
                     <div className="column is-12">
                       <h3 className="has-text-weight-semibold is-size-2">
-                        Upcoming Concerts
+                        Upcoming Events
                       </h3>
                       <FeaturedConcert />
                     </div>
