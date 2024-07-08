9c5ec2cc516aad0960018b9e279cc559a7182570
Jason Awbrey <jawbrey@gmail.com>
2024-07-08 10:16:06 -0500

    disable identity widget

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,7 @@ module.exports = {
       resolve: 'gatsby-plugin-decap-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
+        enableIdentityWidget: false,
       },
     },
     {
